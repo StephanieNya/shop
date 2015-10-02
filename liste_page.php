040f09dcd9bff6928bd8670b2054eca3a4031e60
stephanie <stephygreck@yahoo.fr>
2015-10-02 12:16:19 +0200

    bonjour

---
+++ liste_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+ 
+        // put your code here
+        ?>
+               <h2 id="titre page">Entrées et sorties des marchandises</h2>
+               <div id="image_accueil">
+                   <section>
+                       <h2>Achats de produits de décorations</h2>
+                       <p>Achat de 1 276 pièces au prix moyen de 17.35 euros</p>
+                       <p>Achat de 834 pièces au prix moyen de 38.13 euros</p>
+                   </section>
+                   <section>
+                       <h2>Achats de produits d'ameublements</h2>
+                       <p>Achat de 816 pièces au prix moyen de 83.45 euros</p>
+                       <p>Achat de 609 pièces au prix moyen de 138.71 euros</p>
+                   </section>
+                   
+                   <section>
+                       <h2>contact Responsable des Stocks</h2>
+                       <p>Juliette : extension 18</p>
+                   </section>
+               </div>
+    </body>
+</html>
